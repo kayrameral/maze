9cf44929c8db18781cb8caf3a35fcb36bd000e6a
kayrameral <78321753+kayrameral@users.noreply.github.com>
2022-07-08 18:23:41 +0300

    Update cam.cs

--- labient/Assets/Simple Buttons/cam.cs
+++ labient/Assets/Simple Buttons/cam.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class cam : MonoBehaviour {
 
     public Transform target;
-
+    private float speed = 40f;
     public float smoothSpeed = 12.5f;
     public Vector3 offset;
 
@@ -12,8 +12,17 @@ public class cam : MonoBehaviour {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-
         transform.LookAt(target);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+            offset = transform.position - target.position;
+        }
+
+        if (Input.GetKey(KeyCode.E)){
+                transform.RotateAround(target.position, Vector3.up, -1 * speed * Time.deltaTime);
+            offset = transform.position - target.position;
+        }
     }
 
 }
